29909ba0d852c1592d93b1222caee6a871e9d16a
AkshatIITk <iitkakshat@gmail.com>
2024-05-31 11:46:12 +0530

    added mock test button on hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,11 +20,13 @@ export const Hero = () => {
       <p className='lg:text-sm text-[10px] font-bold mt-2'>Students' Placement Office (SPO) at IIT Kanpur</p>
       <p className="py-6 lg:text-xl text-md font-extralight">A collection of resources for students at IIT Kanpur.</p>
       <div className='lg:flex hidden gap-8 my-8 flex-wrap justify-center items-center mt-5'>
+      <button className="btn btn-outline btn-info rounded-sm"><a href='/mocktest'>Mock Test</a></button>
       <button className="btn btn-outline btn-info rounded-sm"><a href='/insights'>INSIGHTS</a></button>
       <button className="btn btn-outline btn-info rounded-sm"><a onClick={() => handleClick('#PROCEDURES')}>PROCEDURES</a></button>
       <button className="btn btn-outline btn-info rounded-sm"><a onClick={() => handleClick('#RESOURCES')}>RESOURCES</a></button>
       <button className="btn btn-outline btn-info rounded-sm"><a onClick={() => handleClick('#PreparationRoadmaps')}>PREPARATION ROADMAPS</a></button>
       <button className="btn btn-outline btn-info rounded-sm"><a onClick={() => handleClick('#FAQs')}>FAQs</a></button>
+      
       </div>
     </div>
     
